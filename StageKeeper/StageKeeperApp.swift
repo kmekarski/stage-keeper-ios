4563e8095777c54b1f5fc64c65eeb9221deb6670
kmekarski <72306134+kmekarski@users.noreply.github.com>
2024-04-13 12:24:26 +0200

    Initial Commit

---
+++ StageKeeper/StageKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageKeeperApp.swift
+//  StageKeeper
+//
+//  Created by Klaudiusz Mękarski on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct StageKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
